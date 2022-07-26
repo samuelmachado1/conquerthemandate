ee008c8847c02202bfdd09f5bc25dca04d03a0e1
Samuel Machado <samuelmachado1@gmail.com>
2022-07-25 23:40:19 -0300

    develop: Adjusting collision rule with obstacle.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const pipe = document.querySelector('.pipe');
 const player = document.querySelector('.player');
 
 const jump = () => {
@@ -7,4 +8,15 @@ const jump = () => {
     player.classList.remove('jump');
   }, 500)
 }
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  // const playerPosition = window.getComputedStyled(player).bottom.replace('px', '');
+  // console.log('play posit', playerPosition)
+
+  if (pipePosition <= 205) {
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`
+  }
+}, 10)
 document.addEventListener('keydown', jump);
