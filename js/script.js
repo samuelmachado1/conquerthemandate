a540a714d246cb6183e35a312f586cb68efdb836
Samuel Machado <samuelmachado1@gmail.com>
2022-09-03 19:15:54 -0300

    develop: Slightly organizing the structure and testing the sound effect when collecting bonuses.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const dino = document.querySelector(".dino");
+const max = document.querySelector(".max");
 const cacto = document.querySelector(".cacto");
 const catraca = document.querySelector(".catraca");
 const score = document.querySelector(".score");
@@ -9,6 +9,12 @@ const buraco = document.querySelector(".buraco");
 const pastel = document.querySelector(".pastel");
 const combo = document.querySelector(".combo");
 const enemyEntulho = document.querySelector(".enemyEntulho");
+
+
+//audios
+const coin = new Audio("./fx/coin.wav");
+
+
 let alreadyJump = false;
 let count = 0;
 let placar = 0;
@@ -20,12 +26,12 @@ document.addEventListener("keydown", (e) => {
 });
 
 function jump() {
-  if (!dino.classList.contains("jump")) {
-    dino.classList.add("jump");
+  if (!max.classList.contains("jump")) {
+    max.classList.add("jump");
     alreadyJump = true;
 
     setTimeout(() => {
-      dino.classList.remove("jump");
+      max.classList.remove("jump");
       alreadyJump = false;
     }, 1100);
   }
@@ -44,8 +50,8 @@ setInterval(() => {
   let comboLeft = parseInt(
     window.getComputedStyle(combo).getPropertyValue("left")
   );
-  let dinoBottom = parseInt(
-    window.getComputedStyle(dino).getPropertyValue("bottom")
+  let maxBottom = parseInt(
+    window.getComputedStyle(max).getPropertyValue("bottom")
   );
   let cactoLeft = parseInt(
     window.getComputedStyle(cacto).getPropertyValue("left")
@@ -60,32 +66,91 @@ setInterval(() => {
     window.getComputedStyle(buraco).getPropertyValue("left")
   );
 
-  console.log(comboLeft, pastelLeft, dinoBottom);
+  console.log(comboLeft, pastelLeft, maxBottom);
 
-  if (cactoLeft > 40 && cactoLeft < 175 && dinoBottom <= 50 && !alreadyJump) {
-    moedas3.style.left = 80 + "px";
-    count = 0;
-  }
-  if (catracaLeft > -200 && catracaLeft < -100 && dinoBottom <= 50 && !alreadyJump) {
-    alert(`Game Over! Seu score foi: ${placar}`);
-    count = 0;
-  }
-  if (pastelLeft > -50 && pastelLeft < -17 && dinoBottom > 50 && alreadyJump) {
+  // BONUS
+  if (pastelLeft > -50 && pastelLeft < -17 && maxBottom > 50 && alreadyJump) {
     pastel.style.animation = "none";
+    coin.play();
     placar += 122;
   }
-  if (comboLeft > -200 && comboLeft < 0 && dinoBottom > 50 && alreadyJump) {
+  if (comboLeft > -200 && comboLeft < 0 && maxBottom > 50 && alreadyJump) {
     combo.style.animation = "none";
+    coin.play();
     placar += 122;
   }
-  if (enemyEntulhoLeft > -200 && enemyEntulhoLeft < -130 && dinoBottom <= 50 && !alreadyJump) {
-    alert(`Game Over! Seu score foi: ${placar}`);
-    count = 0;
+
+
+
+  // ENEMYS
+
+  if (cactoLeft > 40 && cactoLeft < 175 && maxBottom <= 50 && !alreadyJump) {
+    if (moedas3.style.left == "130px") {
+
+      moedas3.style.left = 0;
+    }
+    if (moedas2.style.left == "80px" && moedas3.style.left != "130px") {
+      moedas2.style.left = 0;
+
+      count = 0;
+    }
+    if (moedas.style.left == "30px" && moedas2.style.left != "80px") {
+      moedas.style.left = 0;
+      alert(`Game Over! Seu score foi: ${placar}`);
+      count = 0;
+    }
   }
-  if (buracoLeft > -200 && buracoLeft < 185 && dinoBottom <= 50 && !alreadyJump) {
-    alert(`Game Over! Seu score foi: ${placar}`);
-    count = 0;
+  if (catracaLeft > -200 && catracaLeft < -100 && maxBottom <= 50 && !alreadyJump) {
+    if (moedas3.style.left == "130px") {
+
+      moedas3.style.left = 0;
+    }
+    if (moedas2.style.left == "80px" && moedas3.style.left != "130px") {
+      moedas2.style.left = 0;
+
+      count = 0;
+    }
+    if (moedas.style.left == "30px" && moedas2.style.left != "80px") {
+      moedas.style.left = 0;
+      alert(`Game Over! Seu score foi: ${placar}`);
+      count = 0;
+    }
   }
+  if (enemyEntulhoLeft > -200 && enemyEntulhoLeft < -130 && maxBottom <= 50 && !alreadyJump) {
+    if (moedas3.style.left == "130px") {
+
+      moedas3.style.left = 0;
+    }
+    if (moedas2.style.left == "80px" && moedas3.style.left != "130px") {
+      moedas2.style.left = 0;
+
+      count = 0;
+    }
+    if (moedas.style.left == "30px" && moedas2.style.left != "80px") {
+      moedas.style.left = 0;
+      alert(`Game Over! Seu score foi: ${placar}`);
+      count = 0;
+    }
+  }
+  if (buracoLeft > -200 && buracoLeft < 185 && maxBottom <= 50 && !alreadyJump) {
+    if (moedas3.style.left == "130px") {
+
+      moedas3.style.left = 0;
+    }
+    if (moedas2.style.left == "80px" && moedas3.style.left != "130px") {
+      moedas2.style.left = 0;
+
+      count = 0;
+    }
+    if (moedas.style.left == "30px" && moedas2.style.left != "80px") {
+      moedas.style.left = 0;
+      alert(`Game Over! Seu score foi: ${placar}`);
+      count = 0;
+    }
+  }
+
+
+  // VICTORY
   if (count > 11800) {
     alert(`VOTE 50100!`);
     count = 0;
