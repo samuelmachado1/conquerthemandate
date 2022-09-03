7a469fb4aa5ea7bf91d26b2ee21847bf287c4387
Samuel Machado <samuelmachado1@gmail.com>
2022-09-03 15:11:53 -0300

    develop: Adding more enemys and adjust bonus points

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@ const cacto = document.querySelector(".cacto");
 const catraca = document.querySelector(".catraca");
 const score = document.querySelector(".score");
 const buraco = document.querySelector(".buraco");
+const pastel = document.querySelector(".pastel");
+const enemyEntulho = document.querySelector(".enemyEntulho");
 let alreadyJump = false;
 let count = 0;
+let placar = 0;
 
 document.addEventListener("keydown", (e) => {
   if ((e.code === "ArrowUp") | (e.code === "Space")) {
@@ -25,6 +28,12 @@ function jump() {
 }
 
 setInterval(() => {
+  let pastelBottom = parseInt(
+    window.getComputedStyle(pastel).getPropertyValue("bottom")
+  );
+  let pastelLeft = parseInt(
+    window.getComputedStyle(pastel).getPropertyValue("left")
+  );
   let dinoBottom = parseInt(
     window.getComputedStyle(dino).getPropertyValue("bottom")
   );
@@ -34,29 +43,43 @@ setInterval(() => {
   let catracaLeft = parseInt(
     window.getComputedStyle(catraca).getPropertyValue("left")
   );
+  let enemyEntulhoLeft = parseInt(
+    window.getComputedStyle(enemyEntulho).getPropertyValue("left")
+  );
   let buracoLeft = parseInt(
     window.getComputedStyle(buraco).getPropertyValue("left")
   );
 
-  console.log(buracoLeft, alreadyJump);
+  console.log(pastelBottom, pastelLeft, dinoBottom);
 
-  if (cactoLeft > 40 && cactoLeft < 120 && dinoBottom <= 50 && !alreadyJump) {
-    alert(`Game Over! Seu score foi: ${count}`);
+  if (cactoLeft > 40 && cactoLeft < 175 && dinoBottom <= 50 && !alreadyJump) {
+    alert(`Game Over! Seu score foi: ${placar}`);
     count = 0;
   }
-  if (catracaLeft > -200 && catracaLeft < -150 && dinoBottom <= 50 && !alreadyJump) {
-    alert(`Game Over! Seu score foi: ${count}`);
+  if (catracaLeft > -200 && catracaLeft < -100 && dinoBottom <= 50 && !alreadyJump) {
+    alert(`Game Over! Seu score foi: ${placar}`);
     count = 0;
   }
-  if (buracoLeft > -200 && buracoLeft < 110 && dinoBottom <= 50 && !alreadyJump) {
-    alert(`Game Over! Seu score foi: ${count}`);
+  if (pastelLeft > -20 && pastelLeft < 0 && dinoBottom > 50 && alreadyJump) {
+    placar.style.animation = "none";
     count = 0;
   }
-  if (count > 13350) {
+  if (enemyEntulhoLeft > -200 && enemyEntulhoLeft < -130 && dinoBottom <= 50 && !alreadyJump) {
+    alert(`Game Over! Seu score foi: ${placar}`);
+    count = 0;
+  }
+  if (buracoLeft > -200 && buracoLeft < 185 && dinoBottom <= 50 && !alreadyJump) {
+    alert(`Game Over! Seu score foi: ${placar}`);
+    count = 0;
+  }
+  if (count > 11800) {
     alert(`VOTE 50100!`);
     count = 0;
   }
-
   count++;
-  score.innerHTML = `SCORE: ${count}`;
+
+  if (count - placar == 100) {
+    placar += 25;
+  }
+  score.innerHTML = `SCORE: ${placar}`;
 }, 10);
